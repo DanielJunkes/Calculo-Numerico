87ae3c6ccfd948ca47813976836b8ed83f1764bc
Thiago Serafina <thiagoserafina@unesc.net>
2024-06-20 18:10:54 -0300

    correção

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Graph from "./components/Graph";
 import { useState } from "react";
 
 function App() {
-  // const [xValues, setXValues] = useState(["", "", "", ""]);
-  // const [yValues, setYValues] = useState(["", "", "", ""]);
-  const [xValues, setXValues] = useState([1.3, 2, 4, 7]);
-  const [yValues, setYValues] = useState([0.5, 1.2, 2, 3]);
+  const [xValues, setXValues] = useState(["", "", "", ""]);
+  const [yValues, setYValues] = useState(["", "", "", ""]);
+  // const [xValues, setXValues] = useState([1.3, 2, 4, 7]);
+  // const [yValues, setYValues] = useState([0.5, 1.2, 2, 3]);
   const [functionType, setFunctionType] = useState("");
   const [result, setResult] = useState("");
 
